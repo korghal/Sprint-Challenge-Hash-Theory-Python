77d92b41d86c4575ea7b9b03fe6fc09471d619e9
Jason Hedrick <jasonleehedrick@gmail.com>
2019-08-04 07:29:04 -0700

    Got ex1.py working with all test cases except [4. 4]. Trying to think of a way to handle this but Ill come back to it.

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -8,13 +8,27 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
+    print(ht.storage)
+    for i in range(0, len(weights)):
+        value1 = limit - weights[i]
+        value2 = weights[i]
+        key1 = hash_table_retrieve(ht, value1)
+        key2 = hash_table_retrieve(ht, value2)
+        if key1 is not None:
+            print(f'key1: {key1} key2: {value2} limit: {limit}')
+            if value1 + value2 == limit:
+                if key1 > key2:
+                    return [key1, key2]
+                else:
+                    return [key2, key1]
+            elif value1 == limit:
+                return [key1]
     return None
 
+
+print(get_indices_of_item_weights([4, 4], 2, 8))
 
 def print_answer(answer):
     if answer is not None:
